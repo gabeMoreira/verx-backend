e008f634f7f67d361dd455c1983bafad8f89b36e
Gabriel Moreira <48988585+gabeMoreira@users.noreply.github.com>
2023-12-10 19:40:55 -0300

    Update producers.js

--- src/routes/producers.js
+++ src/routes/producers.js
@@ -1,7 +1,6 @@
 const controller = require('../controllers/producers');
 const router = require('express').Router();
 
-// CRUD Routes /users
 router.get('/', controller.findAllProducers);
 router.get('/:id', controller.findProducerById);
 router.post('/', controller.createProducer);
